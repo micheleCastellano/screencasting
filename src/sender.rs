3312c988d7a483b6b014ae50c7186d2b5201adbc
Michele Castellano <castellanomichele130@gmail.com>
2024-09-27 02:05:38 +0200

    version 4: changed resolution, but works on mac only

--- src/sender.rs
+++ src/sender.rs
@@ -1,5 +1,4 @@
 use std::vec::Vec;
-use std::thread;
 use std::time::Duration;
 use std::io::Write;
 use std::net::{TcpStream};
@@ -41,7 +40,7 @@ fn scap_init() -> Result<Capturer, ScapError> {
         targets,
         // excluded_targets: None,
         output_type: scap::frame::FrameType::BGRAFrame,
-        // output_resolution: scap::capturer::Resolution::_720p,
+        output_resolution: scap::capturer::Resolution::_480p,
         // source_rect: Some(Area {
         //     origin: Point { x: 0.0, y: 0.0 },
         //     size: Size {
@@ -64,13 +63,13 @@ pub fn send(ip_addr: String) {
     let mut stream = TcpStream::connect(format!("{}:8080", ip_addr)).unwrap();
     println!("Connection successed");
     let frame_number = 0;
-    let fps60 = Duration::new(1, 0) / 60;
+    let _fps60 = Duration::new(1, 0) / 60;
 
     let mut capturer = scap_init().unwrap();
     capturer.start_capture();
 
     loop {
-        thread::sleep(fps60);
+        // thread::sleep(fps60);
         let next_frame = capturer.get_next_frame().unwrap();
         if let Frame::BGRA(mut frame) =  next_frame{
 
