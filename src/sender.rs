ec4b2cf0d0a069ff2aa689523af852bde51b2a24
AngeloBongiorno <angelobongiorno.ab@gmail.com>
2024-10-01 16:21:16 +0200

    added bgrx support

--- src/sender.rs
+++ src/sender.rs
@@ -37,14 +37,14 @@ fn scap_init() -> Result<(), ScapError> {
 }
 
 fn create_capturer(area: Area) -> Capturer {
-    let targets = scap::get_all_targets();
-    println!("{:?}", targets);
+    //let targets = scap::get_all_targets();
+    //println!("{:?}", targets);
     //let targets = scap::get_targets();
     let options = Options {
         fps: 30,
         show_cursor: true,
         show_highlight: false,
-        target: Some(targets.get(0).unwrap().clone()),
+        //target: Some(targets.get(0).unwrap().clone()),
         // excluded_targets: None,
         output_type: scap::frame::FrameType::BGRAFrame,
         output_resolution: scap::capturer::Resolution::_720p,
@@ -58,6 +58,22 @@ fn from_bgra_to_rgba(mut frame: Vec<u8>) -> Vec<u8> {
     for chunk in frame.chunks_exact_mut(4) {
         chunk.swap(0, 2); // Scambia il canale rosso (0) con quello blu (2)
     }
+    frame
+}
+
+fn from_bgrx_to_rgba(mut frame: Vec<u8>) -> Vec<u8> {
+    assert!(
+        frame.len() % 4 == 0,
+        "Frame length must be a multiple of 4!"
+    );
+
+    for chunk in frame.chunks_exact_mut(4) {
+        let blue = chunk[0];
+        let green = chunk[1];
+        let red = chunk[2];
+        chunk.copy_from_slice(&[red, green, blue, 255]);
+    }
+
     frame
 }
 
@@ -101,53 +117,163 @@ pub fn start(ip_addr: String, mut area: Area, msg_r: Receiver<Message>) {
         let next_frame = capturer.get_next_frame().unwrap();
         capturer.stop_capture();
 
-        if let Frame::BGRA(mut frame) = next_frame {
-            // Send header
-            let header = Header::new(
-                frame_number,
-                frame.data.len() as u32,
-                frame.width as u32,
-                frame.height as u32,
-            );
-            let encoded_header: Vec<u8> = bincode::serialize(&header).unwrap();
-
-            if let Err(e) = stream.write(&encoded_header) {
-                println!("Connection closed: {}", e);
-                break 'streaming;
+        match next_frame {
+            Frame::YUVFrame(_) => {
+                println!("yuv");
             }
-            println!(
-                "Header sent {} {}",
-                header.frame_number,
-                SystemTime::now()
-                    .duration_since(SystemTime::UNIX_EPOCH)
-                    .unwrap()
-                    .as_millis()
-            );
-
-            // Send frame
-            frame.data = from_bgra_to_rgba(frame.data);
-            let frame_pad = CHUNK_SIZE - (frame.data.len() as u32 % CHUNK_SIZE);
-            if frame_pad < CHUNK_SIZE {
-                for _ in 0..frame_pad {
-                    frame.data.push(0);
-                }
-            }
-            if let Err(e) = stream.write_all(&frame.data) {
-                println!("Server closed: {}", e);
-                break 'streaming;
-            }
-            println!(
-                "Frame sent {} {}",
-                header.frame_number,
-                SystemTime::now()
-                    .duration_since(SystemTime::UNIX_EPOCH)
-                    .unwrap()
-                    .as_millis()
-            );
-        } else {
-            println!("Sender side: the captured frame format is not supported.");
-            break 'streaming;
+            Frame::RGB(_) => println!("rgb"),
+            Frame::RGBx(_) => println!("rgbx"),
+            Frame::BGRA(_) => println!("bgra"),
+            Frame::XBGR(_) => println!("xbgr"),
+            Frame::BGRx(_) => println!("bgrx"),
+            Frame::BGR0(_) => println!("bgr0"),
         }
+
+        match next_frame {
+            Frame::BGRA(mut frame) => {
+                // Send header
+                let header = Header::new(
+                    frame_number,
+                    frame.data.len() as u32,
+                    frame.width as u32,
+                    frame.height as u32,
+                );
+                let encoded_header: Vec<u8> = bincode::serialize(&header).unwrap();
+
+                if let Err(e) = stream.write(&encoded_header) {
+                    println!("Connection closed: {}", e);
+                    break 'streaming;
+                }
+                println!(
+                    "Header sent {} {}",
+                    header.frame_number,
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis()
+                );
+
+                // Send frame
+                frame.data = from_bgra_to_rgba(frame.data);
+                let frame_pad = CHUNK_SIZE - (frame.data.len() as u32 % CHUNK_SIZE);
+                if frame_pad < CHUNK_SIZE {
+                    for _ in 0..frame_pad {
+                        frame.data.push(0);
+                    }
+                }
+                if let Err(e) = stream.write_all(&frame.data) {
+                    println!("Server closed: {}", e);
+                    break 'streaming;
+                }
+                println!(
+                    "Frame sent {} {}",
+                    header.frame_number,
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis()
+                );
+            }
+            Frame::BGRx(mut frame) => {
+                // Send header
+                let header = Header::new(
+                    frame_number,
+                    frame.data.len() as u32,
+                    frame.width as u32,
+                    frame.height as u32,
+                );
+                let encoded_header: Vec<u8> = bincode::serialize(&header).unwrap();
+
+                if let Err(e) = stream.write(&encoded_header) {
+                    println!("Connection closed: {}", e);
+                    break 'streaming;
+                }
+                println!(
+                    "Header sent {} {}",
+                    header.frame_number,
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis()
+                );
+
+                // Send frame
+                frame.data = from_bgrx_to_rgba(frame.data);
+                //frame.data = from_bgra_to_rgba(frame.data);
+                let frame_pad = CHUNK_SIZE - (frame.data.len() as u32 % CHUNK_SIZE);
+                if frame_pad < CHUNK_SIZE {
+                    for _ in 0..frame_pad {
+                        frame.data.push(0);
+                    }
+                }
+                if let Err(e) = stream.write_all(&frame.data) {
+                    println!("Server closed: {}", e);
+                    break 'streaming;
+                }
+                println!(
+                    "Frame sent {} {}",
+                    header.frame_number,
+                    SystemTime::now()
+                        .duration_since(SystemTime::UNIX_EPOCH)
+                        .unwrap()
+                        .as_millis()
+                );
+            }
+            _ => {
+                println!("Sender side: the captured frame format is not supported.");
+                break 'streaming;
+            }
+        }
+
+        /*
+                if let Frame::BGRA(mut frame) = next_frame {
+                    // Send header
+                    let header = Header::new(
+                        frame_number,
+                        frame.data.len() as u32,
+                        frame.width as u32,
+                        frame.height as u32,
+                    );
+                    let encoded_header: Vec<u8> = bincode::serialize(&header).unwrap();
+
+                    if let Err(e) = stream.write(&encoded_header) {
+                        println!("Connection closed: {}", e);
+                        break 'streaming;
+                    }
+                    println!(
+                        "Header sent {} {}",
+                        header.frame_number,
+                        SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis()
+                    );
+
+                    // Send frame
+                    frame.data = from_bgra_to_rgba(frame.data);
+                    let frame_pad = CHUNK_SIZE - (frame.data.len() as u32 % CHUNK_SIZE);
+                    if frame_pad < CHUNK_SIZE {
+                        for _ in 0..frame_pad {
+                            frame.data.push(0);
+                        }
+                    }
+                    if let Err(e) = stream.write_all(&frame.data) {
+                        println!("Server closed: {}", e);
+                        break 'streaming;
+                    }
+                    println!(
+                        "Frame sent {} {}",
+                        header.frame_number,
+                        SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis()
+                    );
+                } else {
+                    println!("Sender side: the captured frame format is not supported.");
+                    break 'streaming;
+                }
+        */
     }
 
     println!("Sender terminated");
